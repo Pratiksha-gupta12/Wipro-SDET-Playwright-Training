5887c722927386de7ac412c310bf0c2f67e09101
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2026-04-30 15:56:47 +0530

    arrays,loops and if-else basics

--- Day-1/index.js
+++ Day-1/index.js
@@ -48,3 +48,48 @@ function product(a,b){
 
 }
 console.log("Product:", product(3,5))
+
+
+//Array
+
+let arr = [1, 3,"Hey", true, 5];
+let arr2 = [1, 2, 3, 4, 5];
+
+for(let i =0;i< arr.length;i++){
+    console.log(arr[i])
+}
+
+for(let i =0;i< arr.length;i++){
+    console.log(arr[i]*2)
+}
+
+
+arr2.forEach(function(x){
+    console.log(x * 2)
+})
+
+//if-else:
+
+let i = 4;
+let j = 6;
+
+if(i > j) {
+    console.log("i is greater");
+} else {
+    console.log("j is greater");
+}
+
+//Switch
+
+let choice = 1;
+
+switch(choice) {
+    case 1:
+        console.log("Choice is A");
+        break;
+    case 2:
+        console.log("Choice is B");
+        break;  
+    default:
+        console.log("Invalid choice");
+}
