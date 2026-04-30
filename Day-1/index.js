608c82a66852054f3385e330c47b9ccbd3b45a40
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2026-04-30 12:00:11 +0530

    Basic Mathematical, String and Logical Operations
    git push --force

---
+++ Day-1/index.js
@@ -0,0 +1,32 @@
+let a=5;
+let b= 4;
+
+//Mathematical operations
+
+console.log("Addition:", a+b);
+console.log("Subtraction:", a-b);
+console.log("Product:", a*b);
+console.log("Division:", a/b);
+console.log("Remainder:", a%b);
+
+//String operations
+
+let str = "Hey Buddy!";
+
+console.log("Original String:", str);
+console.log("Length of String:", str.length);
+console.log("Uppercase:", str.toUpperCase());
+console.log("Lowercase:", str.toLowerCase());
+console.log("SubString:", str.substring(0,4));
+
+
+
+
+//logical operations
+
+console.log("a > b:", a > b); 
+console.log("a < b:", a < b); 
+console.log("a == b:", a == b); 
+console.log("a != b:", a != b); 
+console.log("a >= b:", a >= b); 
+console.log("a <= b:", a <= b); 
