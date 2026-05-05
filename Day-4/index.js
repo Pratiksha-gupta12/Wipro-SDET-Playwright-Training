286a5770e603e3387aae86a2af290f0e7bf1dbca
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2026-05-05 12:43:49 +0530

    API's

--- Day-4/index.js
+++ Day-4/index.js
@@ -15,27 +15,51 @@
 
 
 
+// function getData(){
+//     return new Promise((resolve,reject) =>{
+//         setTimeout(()=>{
+//             reject("Data received");          
+//         },2000);
+//     });
+// }
+
+// getData().then((data) =>{
+//     console.log(data);
+
+// }).catch((error)=>{
+//     console.log("Error: ", error);
+// });
+
+
+// setTimeout(()=>{
+//     console.log("Hello");
+// },2000);
+
+
+// setInterval(()=>{
+//     console.log("Data checking again and again");
+// },5000);
+
+
+
+
 function getData(){
-    return new Promise((resolve,reject) =>{
-        setTimeout(()=>{
-            reject("Data received");          
-        },2000);
+    return new Promise((resolve, reject)=>{
+        if(2>3){
+            setTimeout(()=>{
+                resolve("Data received");
+            },2000);
+        }else{
+            reject('not working')
+        }
     });
 }
 
-getData().then((data) =>{
-    console.log(data);
 
-}).catch((error)=>{
-    console.log("Error: ", error);
-});
-
-
-setTimeout(()=>{
-    console.log("Hello");
-},2000);
-
-
-setInterval(()=>{
-    console.log("Data checking again and again");
-},5000);
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then(res=> res.json())
+.then(data=> data.filter(d => d.id%2 == 0))
+.then(data => data.map(d=>(d.title = "Hey "+d.title)))
+.then(data =>
+    console.log(data)
+).catch(error => console.log("Error: ",error))
