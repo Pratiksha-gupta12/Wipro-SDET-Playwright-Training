adc7e241b4024be34a928dbad46275eeff29c449
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2026-05-11 12:52:58 +0530

    Typescript basics

---
+++ Day-9/index.ts
@@ -0,0 +1,35 @@
+// let name: string  = "Pratiksha";
+// console.log(name);
+
+
+
+// let result: number | string;
+// result = 45;
+// result = "success";
+// console.log(name, result);
+
+// function add(a: number, b: number): string {
+//     return "Sum: " + (a + b);
+// }
+
+// console.log(add(5, 10));
+
+
+
+let name: string = "Pratiksha";
+name = "Sam";
+console.log(name);
+
+
+let res: number | string = 5;
+res = "Hello"
+console.log(res);
+
+
+function sum(a:number, b: number) : string{
+    return "Sum: " + (a+b);
+}
+console.log(sum(6,5));
+
+
+
