c2b0700c384354058cb5dd67f12d051a346ad408
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2026-05-14 10:47:47 +0530

    Playwright basics

---
+++ Day-12/tests/example.spec.js
@@ -0,0 +1,39 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+
+//test-1
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+
+
+//if we edit playwright to playwright1, it will give error because in the site there is no playwright1 title
+
+
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright1/);
+// });
+
+
+
+//test-2
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
