8816679838e2dc7b0cb5bbc671a190a07afc7844
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2026-05-22 11:31:30 +0530

    fixtures,Parallel and POM

---
+++ Day-19/tests/example.spec.js
@@ -0,0 +1,64 @@
+const { test: base, expect } = require('@playwright/test');
+
+const test = base.extend({
+  preparedPage: async ({ page }, use) => {
+    await page.goto('http://localhost:3000');
+
+    // imagine closing some ads...
+    await use(page);
+  }
+});
+
+// goto -> page [ads popup, promotion banner, forms popup] --> close them --> exec test cases
+
+test.describe.configure({ mode: 'parallel' })
+
+test.describe('Parallel fixtures execution', () => {
+  test.skip('Test 1 running in parallel', async ({ preparedPage }) => {
+    // preparedPage already went to the URL because of our fixture!
+    await expect(preparedPage.getByText('Get started')).toBeVisible();
+  });
+  test.skip('Test 2 running in parallel', async ({ preparedPage }) => {
+    await preparedPage.getByRole('link', { name: 'Get started' }).click();
+    await expect(preparedPage.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+})
+
+
+
+
+
+
+// // @ts-check
+// const {test: base, expect} = require('@playwright/test');
+
+// const test = base.extend({
+//   preparedPage: async ({ page }, use)=>{
+//     await page.goto('http://localhost:3000/');
+
+//     //imagine closing some ads....
+
+//     await use(page);
+//   }
+// });
+
+
+// //goto -> page[ads,popups, promotion banner, forms popup] --> close them --> executing test cases
+
+
+// test.describe.configure({mode: 'parallel'})
+
+// test.describe('Parallel fixtures execution', ()=>{
+//   test.skip('Test 1 running in parallel', async ({ preparedPage})=>{
+
+//     await expect(preparedPage.getByText('Get Started')).toBeVisible();
+//   });
+
+//   test.skip('Test 2 running in parallel', async({ preparedPage})=>{
+//         // preparedPage already went to the URL because of our fixture!
+
+//     await preparedPage.getByRole('link', {name: 'Get Started' }).click();
+
+//     await exepect (preparedPage.getByRole('heading', { name: 'Installation'})).toBeVisible()
+//   });
+// })
